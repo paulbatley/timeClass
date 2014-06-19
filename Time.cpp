4254c9f0d1822c3918ca4545b15acad6efd32bd6
paulbatley <paulbatley@users.noreply.github.com>
2014-06-19 11:30:17 -0700

    Update Time.cpp

--- Time.cpp
+++ Time.cpp
@@ -7,9 +7,24 @@ Time Time::sum(const Time & t) const
 	sum.mins = mins + t.mins;
 	sum.hours = hours + t.hours + sum.mins/60;
 	sum.mins %= 60;
-
-
-
-	return sum;
-
+		return sum;
 }
+	Time Time::operator+(const Time & t) const
+	{
+		Time sum;
+		sum.mins = mins + t.mins;
+		sum.hours = hours + t.hours + sum.mins/60;
+		sum.mins %= 60;
+		return sum;
+	}
+
+	Time Time::operator*(double n) const
+	{
+		Time result;
+		int totalmins = hours*n*60 + mins*n;
+		result.hours = totalmins/60;
+		result.mins = totalmins%60;
+		return result;
+	}
+
+
